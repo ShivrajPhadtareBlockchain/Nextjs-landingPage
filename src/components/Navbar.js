aeee332238870f83699d318585479ce6b7743152
Adesh Nayak <adeshnayak6@gmail.com>
2023-04-26 14:29:28 +0530

    header updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,54 @@
+import { Inter } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 const Navbar = () => {
   return (
     <div className="bg-[#15171A]">
       {/* Top Navbar */}
-      <div className="flex items-center justify-between px-5 py-5">
-        <div></div>
-        <Image src="/NodeShark Default.png" alt="logo" width={50} height={50} />
-        <button className="text-slate-400 hover:text-white font-bold text-xl">Sign in</button>
+      <div className="flex items-center justify-between px-5 py-5 mx-36">
+        <div className="mr-36">
+          <Image src="/search.svg" alt="search_icon" width={20} height={20} />
+        </div>
+        <div className="">
+          <Image
+            src="/NodeShark Default.png"
+            alt="logo"
+            width={40}
+            height={40}
+          />
+        </div>
+        <div className="">
+          <button className={`hover:text-[#ccc] text-white text-base font- mr-2`}>
+            Sign in
+          </button>
+          <button className={`${inter.className} text-[#15171A] bg-white rounded-full  px-4 py-1 font-medium hover:bg-[#f6f6f6] ml-2`}>
+            Subscribe
+          </button>
+        </div>
       </div>
-      <div className="bg-slate-400 w-full h-px"></div>
+      <div className="bg-[#ffffff33] w-full h-px"></div>
       {/* Links Navbar */}
-      <div className="flex items-center justify-center px-5 py-5 shadow-xl">
+      <div className="flex items-center justify-center py-4 shadow-xl">
         <ul className="flex items-center justify-center">
           <Link href="/">
-            <li className="text-slate-400 hover:text-white list-none mx-5 text-lg">Home</li>
+            <li className="hover:text-[#ccc] text-white list-none mx-5 text-sm font-medium">
+              Home
+            </li>
           </Link>
           <Link href="/#about">
-            <li className="text-slate-400 hover:text-white list-none mx-5 text-lg">About us</li>
+            <li className="hover:text-[#ccc] text-white list-none mx-5 text-sm font-medium">
+              About us
+            </li>
           </Link>
           <Link href="/#contact">
-            <li className="text-slate-400 hover:text-white list-none mx-5 text-lg">Contact</li>
+            <li className="hover:text-[#ccc] text-white list-none mx-5 text-sm font-medium">
+              Contact
+            </li>
           </Link>
         </ul>
       </div>
